bc3b8a2967b01abf958604d25bbb04c42945fd5c
TheKaputtmacher <dumkop1@web.de>
2014-03-21 19:14:23 +0100

    Create db_functions.php

---
+++ db_functions.php
@@ -0,0 +1,21 @@
+<?php
+ 
+class DB_Functions {
+ 
+    private $db;
+ 
+    //put your code here
+    // constructor
+    function __construct() {
+        include_once './db_connect.php';
+        // connecting to database
+        $this->db = new DB_Connect();
+        $this->db->connect();
+    }
+ 
+    // destructor
+    function __destruct() {
+         
+    }
+    
+?>
